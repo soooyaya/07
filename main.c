c3f2437a92f265ce0663ab885717fc1443e7ac2a
soooyaya <hsoo0531@naver.com>
2022-10-18 17:08:39 +0900

    6-1

--- main.c
+++ main.c
@@ -2,17 +2,13 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-void f(void);
 
-int i; 
+int all_files;
+
+extern void sub();
+
 int main(int argc, char *argv[]) {
-	for(i=0;i<5;i++){
-		f();
-	}
+	sub();
+	printf("%d\n", all_files);
 	return 0;
-}
-
-void f(void){
-	for(i=0;i<10;i++)
-	printf("#");
 }
